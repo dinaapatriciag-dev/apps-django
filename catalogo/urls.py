009faf81e5c15cf6ec2ffdd1967da914c1fc4451
dinaapatriciag-dev <dinaapatriciag@gmail.com>
2026-06-13 12:51:55 -0300

    aula templates 2

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -5,4 +5,5 @@ app_name = "catalogo"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:id>/", views.detalhes, name="detalhes"),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 
-def index(request):
+obras = [
+    {
+        "id": 1,
+        "titulo": "Interestelar",
+        "tipo": "Filme",
+        "ano": 2014,
+        "genero": "Ficção científica",
+        "descricao": "Um grupo de exploradores viaja através de um buraco de minhoca no espaço."
+    },
+    {
+        "id": 2,
+        "titulo": "Stranger Things",
+        "tipo": "Série",
+        "ano": 2016,
+        "genero": "Suspense",
+        "descricao": "Um grupo de amigos enfrenta acontecimentos sobrenaturais em sua cidade."
+    },
+]
 
-    obras = [
-        {
-            "id": 1,
-            "titulo": "Interestelar",
-            "tipo": "Filme",
-            "ano": 2014,
-            "genero": "Ficção científica",
-            "descricao": "Um grupo de exploradores viaja através de um buraco de minhoca no espaço."
-        },
-        {
-            "id": 2,
-            "titulo": "Stranger Things",
-            "tipo": "Série",
-            "ano": 2016,
-            "genero": "Suspense",
-            "descricao": "Um grupo de amigos enfrenta acontecimentos sobrenaturais em sua cidade."
-        },
-    ]
+def index(request):
 
     context = {
         "obras": obras,
@@ -30,3 +30,14 @@ def index(request):
         "catalogo/index.html",
         context
     )
+
+def detalhes(request, id):
+    obras_encontradas = None
+
+    for obra in obras:
+        if obra["id"] == id:
+            obras_encontradas = obra
+    context = {
+        "obra": obras_encontradas,
+    }
+    return render(request, "catalogo/detalhes.html", context)
